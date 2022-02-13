4ef20ed12699be33e463cc453f50593d80f64eeb
Kate <ekaterinaicenko@gmail.com>
2022-02-13 04:08:44 +0300

    Fix hw_01 and complete medium task

--- hw_1/src/main.py
+++ hw_1/src/main.py
@@ -1,14 +1,17 @@
 import ast
 import inspect
 
-import astunparse as astunparse
 import networkx as nx
 
-from ast_visitor import ASTVisitor
-from fib_numbers import fib_numbers
+from keklib.ast_visitor import ASTVisitor
+from keklib.fib_numbers import fib_numbers
+
+
+def print_ast(f, path):
+    ast_visitor = ASTVisitor()
+    ast_visitor.visit(ast.parse(inspect.getsource(f)))
+    nx.drawing.nx_pydot.to_pydot(ast_visitor.graph).write_png(path)
+
 
 if __name__ == '__main__':
-    ast_visitor = ASTVisitor()
-    ast_visitor.visit(ast.parse(inspect.getsource(fib_numbers)))
-    # print(astunparse.dump(ast.parse(inspect.getsource(fib_numbers))))
-    nx.drawing.nx_pydot.to_pydot(ast_visitor.graph).write_png("../artifacts/fib_num_ast.png")
+    print_ast(fib_numbers, "../artifacts/fib_num_ast.png")
