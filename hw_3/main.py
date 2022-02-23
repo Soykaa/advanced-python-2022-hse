89a64b73b416d744ec932d428c9526d7f251f03c
Kate <ekaterinaicenko@gmail.com>
2022-02-23 20:48:11 +0300

    Complete hard task

--- hw_3/main.py
+++ hw_3/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from hw_3.matrix import Matrix
+from hw_3.mul_mixin import HMatrix
 from hw_3.save_mixin import MMatrix
 
 if __name__ == '__main__':
@@ -22,3 +23,31 @@ if __name__ == '__main__':
     (first_matrix + second_matrix).save_to_file("artifacts/medium/matrix+.txt")
     (first_matrix * second_matrix).save_to_file("artifacts/medium/matrix*.txt")
     (first_matrix @ second_matrix).save_to_file("artifacts/medium/matrix@.txt")
+
+    A = HMatrix([[1, 0], [4, 4]])
+    B = HMatrix([[4, 4], [4, 4]])
+    C = HMatrix([[3, 4], [2, 2]])
+    D = HMatrix([[4, 4], [4, 4]])
+    AB = A @ B
+
+    # clean hashes
+    C._hashes = dict()
+
+    CD = C @ D
+
+    with open("artifacts/hard/A.txt", "w") as file:
+        file.write(A.__str__())
+    with open("artifacts/hard/B.txt", "w") as file:
+        file.write(B.__str__())
+    with open("artifacts/hard/C.txt", "w") as file:
+        file.write(C.__str__())
+    with open("artifacts/hard/D.txt", "w") as file:
+        file.write(D.__str__())
+
+    with open("artifacts/hard/AB.txt", "w") as file:
+        file.write(AB.__str__())
+    with open("artifacts/hard/CD.txt", "w") as file:
+        file.write(CD.__str__())
+    with open("artifacts/hard/hash.txt", "w") as file:
+        file.write("AB:\n" + (AB.__hash__()).__str__() +
+                   "\nCD:\n" + (CD.__hash__()).__str__())
